99a17fdc99c9ba9dfcac1f77ef369235171c8a6e
SeasonalJune <140297872+SeasonalJune@users.noreply.github.com>
2023-07-24 05:16:28 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -2,6 +2,7 @@
 const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg","4.jpg"];
 
 
+function changeBg() {
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
@@ -10,3 +11,5 @@ bgImage.style.backgroundImage = `url(img/${chosenImage})`;
 bgImage.style.backgroundSize = "cover";
 bgImage.style.backgroundPosition = "center";
 document.body.appendChild(bgImage);
+}
+changeBg();
